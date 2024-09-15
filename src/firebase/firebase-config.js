b34ef411051f2076590deffeb901a7a2cd616f11
Tuan Anh <chubatuananh.work@gmail.com>
2024-09-15 11:19:07 +0700

    firebase init

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyB7nXzMPdUH4hmwhZHd3p-CW0VRHWzssYg',
   authDomain: 'messenger-react-anhcbt.firebaseapp.com',
@@ -16,6 +14,8 @@ const firebaseConfig = {
   measurementId: 'G-SKK2CLR8MS',
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
